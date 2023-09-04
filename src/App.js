0a30edd9098c69a2e4309eacb5c06264604392c2
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-09-04 17:44:06 +0530

    initization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          {/* Default redirect to UserForm */}
+          <Route path="/" element={<Home /> } />
+
+          {/* Routes for different components */}
+          {/* <Route path="/user-form" element={<UserForm />} />
+          <Route path="/local-storage" element={<LocalStoragePage />} />
+          <Route path="/firebase" element={<FirebasePage />} /> */}
+        </Routes>
+    </Router>
   );
 }
 
